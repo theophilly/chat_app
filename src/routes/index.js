5f88d22387c99253c2d855c64e327caee00e846a
Adeyemi Kolade <theophilly20@gmail.com>
2022-08-17 05:31:59 -0700

    added display name to messages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
-import { useRoutes } from 'react-router-dom';
-import MessagesScreen from '../pages/messages';
-
+import { useRoutes } from "react-router-dom";
+import MessagesScreen from "../pages/messages";
+import Homepage from "../pages/homepage";
 
 export default function ThemeRoutes() {
-
-    return useRoutes([{
-        path: '/',
-        element: <MessagesScreen />}]);
+  return useRoutes([
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "/messages",
+      element: <MessagesScreen />,
+    },
+  ]);
 }
