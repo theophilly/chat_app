9681e6252bbe221cd63f825bb01a71db952130cb
Adeyemi Kolade <theophilly20@gmail.com>
2022-08-17 02:34:00 -0700

    loading messages from store

--- src/pages/messages/Message.js
+++ src/pages/messages/Message.js
@@ -6,7 +6,7 @@ import {
     Box,
 } from '@mui/material';
 
-export default function Message({ owner = false }) {
+export default function Message({ owner = false, message }) {
     const theme = useTheme();
     return (
         <>
@@ -15,7 +15,7 @@ export default function Message({ owner = false }) {
                     <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                         <Avatar alt="Remy Sharp" src="./samantha.jpg" sx={{ width: 35, height: 35, mr: '10px', mb: '3px' }} />
                         <Typography sx={{ bgcolor: '#CEE9FF', padding: '15px', borderRadius: '10px', width: '500px' }}>
-                            Hi, hope you had enough rest during the weekend? Let’s talk when you are free today?
+                         {message}
                         </Typography>
                     </Box>
                     <Typography sx={{ alignSelf: 'flex-end' }} variant="caption">
@@ -28,7 +28,8 @@ export default function Message({ owner = false }) {
                 <Box sx={{ ...theme.typography.column, alignItems: 'flex-end', mt: '10px' }}>
                     <Box sx={{ width: '500px' }}>
                         <Typography sx={{ bgcolor: '#D6D6D6', padding: '15px', borderRadius: '10px', width: '500px' }}>
-                            Hi, hope you had enough rest during the weekend? Let’s talk when you are free today?
+                            {/* Hi, hope you had enough rest during the weekend? Let’s talk when you are free today? */}
+                       {message}
                         </Typography>
 
                         <Typography sx={{ textAlign: 'start' }} variant="caption">
