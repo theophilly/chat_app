13d47d5fc98df84f4dc337071e0d8309b22d44d2
theophilly <theophilly20@gmail.com>
2022-08-30 14:57:26 -0700

    Code cleanups (#16)
    * adding new user notification
    
    * testing msg data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+} from "@mui/material";
+import "./App.css";
 
-import theme from './theme';
+import theme from "./theme";
 
-
-import Routes from './routes';
-import ScrollToTop from './components/ScrollToTop';
+import Routes from "./routes";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-    return (
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <ScrollToTop />
-                <Routes />
-            </ThemeProvider>
-        </StyledEngineProvider>
-    );
+  const [color, setColor] = useState("red");
+
+  //   const setMessage = () => {
+  //     window.postMessage("green");
+  //   };
+
+  const reactNativeWebviewListener = (msg) => {
+    if (typeof msg.data === "object") return;
+    setColor(msg.data);
+    // console.log(msg);
+  };
+
+  useEffect(() => {
+    window.addEventListener("message", reactNativeWebviewListener);
+
+    return () => {
+      window.removeEventListener("message", reactNativeWebviewListener);
+    };
+  }, []);
+  return (
+    <StyledEngineProvider injectFirst>
+      <div
+        // onClick={setMessage}
+        style={{ width: "100px", height: "100px", background: color }}
+      ></div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <ScrollToTop />
+        <Routes />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
 
 export default App;
