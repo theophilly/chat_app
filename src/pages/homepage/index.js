9d25a78b5431f033b26622177449ca41ae9a8415
theophilly <theophilly20@gmail.com>
2022-09-10 11:40:23 -0700

    Code cleanups (#17)
    * adding new user notification
    
    * testing msg data
    
    * refactor
    
    * refactor

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,39 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, InputBase, useTheme, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useHomeActions } from "../../hooks/useHomeActions";
 
 export default function Homepage() {
   const theme = useTheme();
-  const [name, setName] = useState("");
-  let navigate = useNavigate();
-  const dispatch = useDispatch();
 
-  const onTextChange = (e) => {
-    const { value } = e.target;
-    if (value.trim()) {
-      sessionStorage.setItem("profileName", value.toLowerCase());
-      setName(value.toLowerCase());
-    }
-  };
-
-  const navigateToMessages = async () => {
-    if (name) {
-      await dispatch({
-        type: "ADD_USER",
-        payload: {
-          user: name,
-        },
-      });
-      navigate("/messages", { replace: true });
-    }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      navigateToMessages();
-    }
-  };
+  const {
+    onTextChange,
+    navigateToMessages,
+    handleKeyDown,
+    name,
+  } = useHomeActions();
 
   return (
     <Box
