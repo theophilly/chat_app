1c0a6db1a33f0f7fd4fbd7882f621461e9992644
Adeyemi Kolade <theophilly20@gmail.com>
2022-09-10 19:30:57 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   CssBaseline,
   StyledEngineProvider,
@@ -12,31 +12,8 @@ import Routes from "./routes";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  const [color, setColor] = useState("red");
-
-  //   const setMessage = () => {
-  //     window.postMessage("green");
-  //   };
-
-  const reactNativeWebviewListener = (msg) => {
-    if (typeof msg.data === "object") return;
-    setColor(msg.data);
-    // console.log(msg);
-  };
-
-  useEffect(() => {
-    window.addEventListener("message", reactNativeWebviewListener);
-
-    return () => {
-      window.removeEventListener("message", reactNativeWebviewListener);
-    };
-  }, []);
   return (
     <StyledEngineProvider injectFirst>
-      <div
-        // onClick={setMessage}
-        style={{ width: "100px", height: "100px", background: color }}
-      ></div>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <ScrollToTop />
